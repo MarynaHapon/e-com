ef6dd80c423c8cbc318e7af1cebaee3c97987d81
Maryna Hapon <marynahapon@gmail.com>
2020-06-13 11:05:06 +0300

    update directory url

--- src/redux/directory/data.js
+++ src/redux/directory/data.js
@@ -3,32 +3,32 @@ export const data = [
     title: 'Hats',
     imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
     id: 1,
-    linkUrl: 'shop/hats',
+    linkUrl: '/shop/hats',
   },
   {
     title: 'Sneakers',
     imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
     id: 2,
-    linkUrl: 'shop/sneakers'
+    linkUrl: '/shop/sneakers'
   },
   {
     title: 'Jackets',
     imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
     id: 3,
-    linkUrl: 'shop/jackets'
+    linkUrl: '/shop/jackets'
   },
   {
     title: 'Womens',
     imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
     size: 'large',
     id: 4,
-    linkUrl: 'shop/womens'
+    linkUrl: '/shop/womens'
   },
   {
     title: 'Mens',
     imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
     size: 'large',
     id: 5,
-    linkUrl: 'shop/mens'
+    linkUrl: '/shop/mens'
   }
 ];
