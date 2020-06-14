5a7f7046669683ed20501ba5cf886718dec297c2
Maryna Hapon <marynahapon@gmail.com>
2020-06-14 22:43:05 +0300

    update components

--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -6,7 +6,7 @@ import { Route } from 'react-router-dom';
 import { CollectionPage } from '../../pages';
 
 // Components
-import { CollectionOverview } from '../../components';
+import { CollectionOverview, Shape } from '../../components';
 
 // Other
 import './index.styles.scss';
@@ -14,6 +14,8 @@ import './index.styles.scss';
 export const ShopPage = ({ match: { path } }) => {
   return (
     <div className='shopPage'>
+      <Shape type='primary' width={65} height={100} top={-5} left={-10} />
+      <Shape type='secondary' width={60} height={80} bottom={20} right={-20} />
       <Route exact path={path} component={CollectionOverview} />
       <Route path={`${path}/:collectionId`} component={CollectionPage} />
     </div>
