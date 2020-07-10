f967bcd46378bd0295d79c75b07668663f109e7f
Maryna Hapon <marynahapon@gmail.com>
2020-07-10 08:33:56 +0300

    add saga init

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,25 @@
 // Core
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga'
 import { persistStore } from 'redux-persist';
 
 // Other
 import { persistRoodReducer } from '../redux/rootReducer';
 
-const middlewares = [ thunk ];
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [ sagaMiddleware ];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
+// sagaMiddleware.run();
+
 export const store = createStore(persistRoodReducer, applyMiddleware(
   ...middlewares
 ));
+
 
 export const persistor = persistStore(store);
